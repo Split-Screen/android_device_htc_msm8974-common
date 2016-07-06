3c806791b43e15b7d4368731990d49b9b520b349
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-06 09:32:05 -0500

    msm8974: add gesture handler for htc gestures
    * This is taken from the SlimRoms implementation of DeviceHandler
      and adapted as a service here. We'll control it through framework.
    
    * TODO: add custom actions to swipe gestures
    
    Change-Id: Ifa5c193ff6501976b33952040c6449a176ae699d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,4 +132,3 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # Hardware
 BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/htc_sensorhub/sensor_hub/tap2wake"
